45faa20c6ab50d1eeb4a51acf190d23d2193cfa3
Mark Fisher <mark.j.fisher@gmail.com>
2024-01-29 18:10:17 +0000

    Running Cn00 working - fixes PR#N

--- firmware/SPoverSLIP/spoverslip.s
+++ firmware/SPoverSLIP/spoverslip.s
@@ -1,5 +1,4 @@
 ; FUJINET firmware
-; TBD: Is this also a full SP implementation for AppleWin?
 ;
 ; cl65 -t none fujinet.s -o fujinet.bin
 
@@ -8,6 +7,13 @@ loc1    := $01
 
 spmagic := $65          ; magic byte to trigger SmartPort code in Emulator
 pdmagic := $66          ; magic byte to trigger ProDos code in Emulator
+
+cmd     := $42          ; disk driver addresses, used during direct boot
+unit    := $43
+bufl    := $44
+bufh    := $45
+blockl  := $46
+blockh  := $47
 
 mslot   := $07F8        ; BUFFER FOR HI SLOT ADDR (SCN)
 
@@ -29,8 +35,31 @@ header:
         cpx     #$03
         cpx     #$00
 
-        ; if we're called directly (e.g. PR#5) just print an error and drop to basic.
-        ; this is a copy of Oliver Schmidt code for Reload Emulator at https://github.com/vsladkov/reload-emulator/pull/1/files#diff-b889f9e1f8b8d8898b043495b2e25bfc19a9b12655456af92ebab9f92c9e20b0
+        ; this is a copy of Oliver Schmidt code for Reload Emulator at 
+        ; https://github.com/vsladkov/reload-emulator/pull/1/files#diff-b889f9e1f8b8d8898b043495b2e25bfc19a9b12655456af92ebab9f92c9e20b0
+  
+        stx        blockl
+        stx        blockh
+        stx        bufl
+        inx                     ; cmd = 1 => read
+        stx        cmd
+        ldx        #>$0800      ; high byte of read buffer location
+        stx        bufh 
+n16_1:
+        ldx        #$00         ; replaced by Card with slot * 16 == Drive 1 indicator
+        stx        unit
+cn3:
+        jsr        driver       ; need to adjust the location as it compiles to c0, but need cN
+        bcs        do_err
+        ldx        $0800        ; disk II sector count
+        dex
+        bne        do_err       ; must be 1 sector
+        ldx        $0801        ; first opcode from read sector
+        beq        do_err       ; must not be BRK
+n16_2:
+        ldx        #$00         ; replaced by Card with slot * 16 == Drive 1 indicator
+        jmp        $0801        ; run, implicit RTS at the end will return to caller
+
 do_err:
         ldx     loc0
         bne     errexit
@@ -82,9 +111,12 @@ errtext:
         .byte 'R'|$80, 'O'|$80, 'R'|$80, 'R'|$80, 'E'|$80, ' '|$80, 'N'|$80, 'F'|$80
 
         ; write data to the end of the block
-        .res    $0100-7-<*
-        .byte   <(cn1+1)         ; high byte of 0xCn00, needed in error routine #1
-        .byte   <(cn2+2)         ; high byte of 0xCn00, needed in error routine #2
+        .res    $0100-10-<*
+        .byte   <(n16_1+1)      ; slot * 16 for direct boot #1
+        .byte   <(n16_2+1)      ; slot * 16 for direct boot #2
+        .byte   <(cn1+1)        ; high byte of 0xCn00, needed in error routine #1 (SINGLE BYTE)
+        .byte   <(cn2+2)        ; high byte of 0xCn00, needed in error routine #2 (HIGH BYTE of pair)
+        .byte   <(cn3+2)        ; high byte of 0xCn00, needed in jsr driver (HIGH BYTE of pair)
         .byte   <(n2+1)         ; low byte of 0xC0n2 address for initiating the SP command
         .byte   $00, $00        ; total blocks, causes status to be called to get value
         .byte   $f7             ; status bits (http://www.easy68k.com/paulrsm/6502/PDOS8TRM.HTM)
